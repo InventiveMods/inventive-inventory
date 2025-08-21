732c16ea50723bd35f7061e46d72e2c717dd6cd0
LU-XD <114474715+LU-XD@users.noreply.github.com>
2025-08-21 09:55:59 +0200

    1.3.0

---
+++ src/main/java/net/inventive_mods/inventive_inventory/features/item_counter/ItemCounterHudElement.java
@@ -0,0 +1,37 @@
+package net.inventive_mods.inventive_inventory.features.item_counter;
+
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.inventive_mods.inventive_inventory.config.ConfigManager;
+import net.inventive_mods.inventive_inventory.config.enums.Status;
+import net.inventive_mods.inventive_inventory.config.enums.item_counter.ItemCounterMode;
+import net.inventive_mods.inventive_inventory.util.Drawer;
+import net.inventive_mods.inventive_inventory.util.InteractionHandler;
+import net.inventive_mods.inventive_inventory.util.slots.PlayerSlots;
+import net.inventive_mods.inventive_inventory.util.slots.SlotTypes;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
+public class ItemCounterHudElement implements HudRenderCallback {
+    @Override
+    public void onHudRender(DrawContext context, float tickCounter) {
+        if (ConfigManager.ITEM_COUNTER_STATUS.is(Status.DISABLED))
+            return;
+
+        List<ItemStack> inventory = (ConfigManager.ITEM_COUNTER_IGNORE_LOCKED_SLOTS.is(true) ? PlayerSlots.get().exclude(SlotTypes.LOCKED_SLOT) : PlayerSlots.get()).stream().map(InteractionHandler::getStackFromSlot).toList();
+        int slotX = context.getScaledWindowWidth() / 2 - 90 + 2;
+        int slotY = context.getScaledWindowHeight() - 16 - 3;
+        for (Integer slot : PlayerSlots.get(SlotTypes.HOTBAR)) {
+            ItemStack stack = InteractionHandler.getStackFromSlot(slot);
+            if (!stack.isEmpty() && (ConfigManager.ITEM_COUNTER_MODE.is(ItemCounterMode.COMPLETE_HOTBAR) || ConfigManager.ITEM_COUNTER_MODE.is(ItemCounterMode.ONLY_SELECTED_SLOT) && InteractionHandler.getSelectedSlot() == slot)) {
+                int count = inventory.stream()
+                        .filter(itemStack -> ItemStack.areItemsEqual(itemStack, stack))
+                        .mapToInt(ItemStack::getCount)
+                        .sum();
+                Drawer.drawItemCounter(context, slotX, slotY, count, stack);
+            }
+            slotX += 20;
+        }
+    }
+}
