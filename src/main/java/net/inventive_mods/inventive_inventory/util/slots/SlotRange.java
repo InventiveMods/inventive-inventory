70fad8120a630e9e302fd6b7e4ac39dd1980d94b
LU-XD <114474715+LU-XD@users.noreply.github.com>
2025-10-16 21:48:24 +0200

    1.3.3

--- src/main/java/net/inventive_mods/inventive_inventory/util/slots/SlotRange.java
+++ src/main/java/net/inventive_mods/inventive_inventory/util/slots/SlotRange.java
@@ -26,6 +26,10 @@ public class SlotRange extends ArrayList<Integer> {
         return new SlotRange(list);
     }
 
+    public static SlotRange empty() {
+        return new SlotRange(0, -1);
+    }
+
     public SlotRange append(SlotTypes type) {
         if (type == SlotTypes.HOTBAR) {
             ScreenHandler screenHandler = InventiveInventory.getScreenHandler();
@@ -37,6 +41,10 @@ public class SlotRange extends ArrayList<Integer> {
             if (playerSlots.stream().anyMatch(slot -> slot.getClass().equals(Slot.class))) {
                 playerSlots = playerSlots.stream().filter(slot -> slot.getClass().equals(Slot.class)).toList();
             }
+
+            if (playerSlots.isEmpty())
+                return this;
+
             int start = playerSlots.get(0).id;
             int stop = playerSlots.get(playerSlots.size() - 1).id;
             IntStream.rangeClosed(start, stop).forEach(this::add);
