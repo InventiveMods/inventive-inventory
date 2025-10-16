6842f4a7d1f0e27012f3b43abd60afbed71c7965
LU-XD <114474715+LU-XD@users.noreply.github.com>
2025-10-16 20:37:36 +0200

    1.3.4

--- src/main/java/net/inventive_mods/inventive_inventory/features/item_counter/ItemCounterHudElement.java
+++ src/main/java/net/inventive_mods/inventive_inventory/features/item_counter/ItemCounterHudElement.java
@@ -12,7 +12,6 @@ import net.inventive_mods.inventive_inventory.util.slots.SlotTypes;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.LecternScreenHandler;
 import net.minecraft.util.Identifier;
 
 import java.util.List;
@@ -22,7 +21,7 @@ public class ItemCounterHudElement implements HudElement {
 
     @Override
     public void render(DrawContext context, RenderTickCounter tickCounter) {
-        if (ConfigManager.ITEM_COUNTER_STATUS.is(Status.DISABLED) || (InventiveInventory.getPlayer() != null && InventiveInventory.getPlayer().isCreative()) || InventiveInventory.getScreenHandler() instanceof LecternScreenHandler)
+        if (ConfigManager.ITEM_COUNTER_STATUS.is(Status.DISABLED) || (InventiveInventory.getPlayer() != null && InventiveInventory.getPlayer().isCreative()))
             return;
 
         List<ItemStack> inventory = (ConfigManager.ITEM_COUNTER_IGNORE_LOCKED_SLOTS.is(true) ? PlayerSlots.get().exclude(SlotTypes.LOCKED_SLOT) : PlayerSlots.get()).stream().map(InteractionHandler::getStackFromSlot).toList();
